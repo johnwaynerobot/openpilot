10fe7c5a0e74aba2a8cd7632b4894694c4d28f09
DV <dvu@columbus.ai>
2018-09-03 18:13:03 -0400

    test2 kia

---
+++ selfdrive/car/kia/kiacan.py
@@ -0,0 +1,214 @@
+import struct
+
+import common.numpy_fast as np
+from selfdrive.config import Conversions as CV
+ #2018.09.03 DV modified remove specific car
+
+# *** Honda specific ***
+def can_cksum(mm):
+  s = 0
+  for c in mm:
+    c = ord(c)
+    s += (c>>4)
+    s += c & 0xF
+  s = 8-s
+  s %= 0x10
+  return s
+
+
+def fix(msg, addr):
+  msg2 = msg[0:-1] + chr(ord(msg[-1]) | can_cksum(struct.pack("I", addr)+msg))
+  return msg2
+
+
+def make_can_msg(addr, dat, idx, alt):
+  if idx is not None:
+    dat += chr(idx << 4)
+    dat = fix(dat, addr)
+  return [addr, 0, dat, alt]
+
+#2018.09.01 this one we should add fingerprint CAR.SOUL ,
+def create_brake_command(packer, apply_brake, pcm_override, pcm_cancel_cmd, chime, fcw, idx):
+  """Creates a CAN message for the Honda DBC BRAKE_COMMAND."""
+  brakelights = apply_brake > 0
+  pcm_fault_cmd = False
+
+  values = {
+    "CRUISE_OVERRIDE": pcm_override,
+    "CRUISE_FAULT_CMD": pcm_fault_cmd,
+    "CRUISE_CANCEL_CMD": pcm_cancel_cmd,
+    "SET_ME_0X80": 0x80,
+    "BRAKE_LIGHTS": brakelights,
+    "CHIME": chime,
+    "FCW": fcw << 1,  # TODO: Why are there two bits for fcw? According to dbc file the first bit should also work
+  }
+  return packer.make_can_msg("BRAKE_COMMAND", 0, values, idx)
+
+def create_brake_command_soul(packer, apply_brake,):
+  """Creates a CAN message for the Honda DBC BRAKE_COMMAND."""
+  brake_rq = apply_brake > 0
+  if brake_rq==1:
+    x=0xCC05
+
+  values = {
+    "BRAKE_COMMAND_pedal_command": apply_brake,   # computer
+    "BRAKE_COMMAND_magic": x ,
+  }
+  return packer.make_can_msg("SOUL_BRAKE_COMMAND", 0, values)  #remove idx no need for alive counter and checksum
+
+def create_brake_enable_soul(packer, apply_brake, idx):
+  """Creates a CAN message for the Honda DBC BRAKE_COMMAND."""
+
+  brake_rq = apply_brake > 0
+
+  if brake_rq==1:
+    x=0xCC05
+
+  values = {
+    "BRAKE_ENABLE_magic": x,
+  }
+  return packer.make_can_msg("SOUL_BRAKE_ENABLE", 0, values) #remove idx no need for alive counter and checksum
+
+def create_brake_disable_soul(packer, apply_brake,  idx):
+  """Creates a CAN message for the Honda DBC BRAKE_COMMAND."""
+  brake_rq = apply_brake > 0
+
+  if brake_rq==0:
+    x=0xCC05
+
+  values = {
+    "BRAKE_DISABLE_magic": x,
+  }
+  return packer.make_can_msg("SOUL_BRAKE_DISABLE", 0, values) #remove idx no need for alive counter and checksum
+
+
+def create_gas_command(packer, gas_amount, idx):
+  """Creates a CAN message for the Honda DBC GAS_COMMAND."""
+  enable = gas_amount > 0.001
+
+  values = {} #initializing the value dict empty initially
+  if enable ==1:
+    x_gas=0xCC05
+
+  if enable:
+    values["THROTTLE_COMMAND_magic"] = x_gas
+    values["THROTTLE_COMMAND_pedal_command"] = gas_amount 
+
+  return packer.make_can_msg("THROTTLE_COMMAND", 0, values) #remove idx no need for alive counter and checksum
+
+def create_gas_command_enable(packer, gas_amount):
+  """Creates a CAN message for the Honda DBC GAS_COMMAND."""
+  enable = gas_amount > 0.001
+
+  values = {} #initializing the value dict empty initially
+  if enable ==1:
+    x_gas_enable=0xCC05 
+
+  if enable:
+    values["THROTTLE_ENABLE_magic"] = x_gas_enable
+
+  return packer.make_can_msg("THROTTLE_ENABLE", 0, values) #remove idx no need for alive counter and checksum
+
+def create_gas_command_disable(packer, gas_amount):
+  """Creates a CAN message for the Honda DBC GAS_COMMAND."""
+  disable = gas_amount < 0.001
+
+  values = {} #initializing the value dict empty initially
+  if disable == 1:
+    x_gas_disable=0xCC05 
+
+  if disable:
+    values["THROTTLE_DISABLE_magic"] = x_gas_disable
+
+  return packer.make_can_msg("THROTTLE_DISABLE", 0, values) #remove idx no need for alive counter and checksum
+
+
+def create_steering_control(packer, apply_steer, lkas_active):
+  """Creates a CAN message for the Honda DBC STEERING_CONTROL."""
+  if lkas_active==1:
+    x_steering_enable=0xCC05
+  values = {
+    "STEERING_COMMAND_magic": apply_steer if lkas_active else 0,
+    "STEERING_COMMAND_pedal_command": x_steering_enable,
+  }
+  return packer.make_can_msg("STEERING_COMMAND", 0 , values) #remove idx no need for alive counter and checksum
+
+def create_steering_control_enable(packer,lkas_active):
+  """Creates a CAN message for the Honda DBC STEERING_CONTROL."""
+  if lkas_active==1:
+    x_steering_enable=0xCC05
+
+  values= {
+    "STEERING_ENABLE_magic": x_steering_enable
+    }
+  return packer.make_can_msg("STEERING_ENABLE", 0, values) #remove idx no need for alive counter and checksum
+
+def create_steering_control_disable(packer, lkas_active):
+  """Creates a CAN message for the Honda DBC STEERING_CONTROL."""
+  if lkas_active==0:
+    x_steering_disable=0xCC05
+  values = {
+    "STEERING_DISABLE_magic": x_steering_disable
+  }
+  return packer.make_can_msg("STEERING_DISABLE", 0, values) #remove idx no need for alive counter and checksum
+
+
+
+def create_ui_commands(packer, pcm_speed, hud):
+  """Creates an iterable of CAN messages for the UIs."""
+  commands = []
+
+  acc_hud_values = {
+      'PCM_SPEED': pcm_speed * CV.MS_TO_KPH,
+      'PCM_GAS': hud.pcm_accel,
+      'CRUISE_SPEED': hud.v_cruise,
+      'ENABLE_MINI_CAR': hud.mini_car,
+      'HUD_LEAD': hud.car,
+      'SET_ME_X03': 0x03,
+      'SET_ME_X03_2': 0x03,
+      'SET_ME_X01': 0x01,
+  }
+  commands.append(packer.make_can_msg("ACC_HUD", 0, acc_hud_values, idx))
+
+  lkas_hud_values = {
+    'SET_ME_X41': 0x41,
+    'SET_ME_X48': 0x48,
+    'STEERING_REQUIRED': hud.steer_required,
+    'SOLID_LANES': hud.lanes,
+    'BEEP': hud.beep,
+  }
+  commands.append(packer.make_can_msg('LKAS_HUD', 0, lkas_hud_values, idx))
+
+
+  radar_hud_values = {
+      'ACC_ALERTS': hud.acc_alert,
+      'LEAD_SPEED': 0x1fe,  # What are these magic values
+      'LEAD_STATE': 0x7,
+      'LEAD_DISTANCE': 0x1e,
+  }
+  commands.append(packer.make_can_msg('RADAR_HUD', 0, radar_hud_values, idx)) #2018.09.03 change to bus 0
+  return commands
+
+
+def create_radar_commands(v_ego, idx):
+  """Creates an iterable of CAN messages for the radar system."""
+  commands = []
+  v_ego_kph = np.clip(int(round(v_ego * CV.MS_TO_KPH)), 0, 255)
+  speed = struct.pack('!B', v_ego_kph)
+
+  msg_0x300 = ("\xf9" + speed + "\x8a\xd0" +
+               ("\x20" if idx == 0 or idx == 3 else "\x00") +
+               "\x00\x00")
+
+  msg_0x301 = "\x00\x00\x50\x02\x51\x00\x00"
+  commands.append(make_can_msg(0x300, msg_0x300, idx, 1))
+
+  commands.append(make_can_msg(0x301, msg_0x301, idx, 1))
+  return commands
+
+def spam_buttons_command(packer, button_val, idx):
+  values = {
+    'CRUISE_BUTTONS': button_val,
+    'CRUISE_SETTING': 0,
+  }
+  return packer.make_can_msg("SCM_BUTTONS", 0, values, idx)
---
+++ selfdrive/car/kia/values.py
@@ -0,0 +1,69 @@
+from selfdrive.car import dbc_dict
+
+# Car button codes
+class CruiseButtons:
+  RES_ACCEL   = 4
+  DECEL_SET   = 3
+  CANCEL      = 2
+  MAIN        = 1
+
+
+#car chimes: enumeration from dbc file. Chimes are for alerts and warnings
+class CM:
+  MUTE = 0
+  SINGLE = 3
+  DOUBLE = 4
+  REPEATED = 1
+  CONTINUOUS = 2
+
+
+#car beepss: enumeration from dbc file. Beeps are for activ and deactiv
+class BP:
+  MUTE = 0
+  SINGLE = 3
+  TRIPLE = 2
+  REPEATED = 1
+
+class AH:
+  #[alert_idx, value]
+  # See dbc files for info on values"
+  NONE           = [0, 0]
+  FCW            = [1, 1]  #2018.09.02 have
+  STEER          = [2, 1]   #2018.09.02 TODO check alert values
+  BRAKE_PRESSED  = [3, 10]  #2018.09.02 value does match
+  GEAR_NOT_D     = [4, 6]  #2018.09.02 TODO this come from BO_927 0x39F from adas need to add in DBC
+  SEATBELT       = [5, 5]  #2018.09.02 not find where it use
+  SPEED_TOO_HIGH = [6, 8]  #2018.09.02 TODO this come from BO_927 0x39F from adas need to add in DBC
+
+
+class CAR:
+  SOUL = "KIA SOUL TEST" #Test Spoof DV 2018.09.02
+  DUMMY = "DUMMY TEST SPOOF" #2018.09.03 test spoof
+
+
+FINGERPRINTS = {
+  # Soul Vehicle with Steering Gas and Throttle in disable state
+  CAR.SOUL: [{
+    115: 8, 131: 8, 147: 8, 185: 8, 357: 8, 422: 8, 506: 8, 544: 8, 688: 8, 780: 8, 790: 8, 809: 8, 829: 5, 880: 8, 1088: 8, 1200: 8, 1680: 8
+  }],
+  CAR.DUMMY: [{
+    57: 3, 145: 8, 228: 5, 304: 8, 316: 8, 342: 6, 344: 8, 380: 8, 398: 3, 399: 7, 419: 8, 420: 8, 422: 8, 428: 8, 432: 7, 464: 8, 476: 4, 490: 8, 506: 8, 512: 6, 513: 6, 542: 7, 545: 4, 597: 8, 660: 8, 773: 7, 777: 8, 780: 8, 800: 8, 804: 8, 808: 8, 819: 7, 821: 5, 829: 5, 882: 2, 884: 7, 887: 8, 888: 8, 892: 8, 923: 2, 929: 4, 983: 8, 985: 3, 1024: 5, 1027: 5, 1029: 8, 1030: 5, 1034: 5, 1036: 8, 1039: 8, 1057: 5, 1064: 7, 1108: 8, 1365: 5,
+  }],
+}
+
+
+DBC = {
+  CAR.SOUL: dbc_dict('kia_soul_2016_pt_generated.dbc', 'acura_ilx_2016_nidec'), # 2018.09.03 DV change to generator.py dbc
+}
+
+
+STEER_THRESHOLD = {
+  CAR.SOUL: 0, #2018.09.02 D.V. steering override from OSCC car in carstate.py when 1 is override
+}
+
+SPEED_FACTOR = {
+  CAR.SOUL: 1., #2018.09.02 DV add Soul vehicle for test
+}
+
+# TODO: get these from dbc file
+HONDA_BOSCH = [CAR.DUMMY]   #2018.09.03 Define car dummy for test
